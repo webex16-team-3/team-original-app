3a6cf01f1d5620748c683df528375f1897e5d1e4
Yuji <yuji08752@gmail.com>
2022-11-27 19:56:25 +0900

    .prettierrcの作成

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // 必要な関数を import
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfVm963UtQhAqOtlYSO9UrXaaj1VJjQzY",
@@ -10,9 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "1027978208428",
   appId: "1:1027978208428:web:fb2aaab54d4ad0d652eb09",
   measurementId: "G-C6SX9J0MHL",
-};
+}
 
 // Firebaseアプリオブジェクトを初期化
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 // Firestoreを読み込み、db(databaseの略)として export
-export const db = getFirestore(app);
+export const db = getFirestore(app)
