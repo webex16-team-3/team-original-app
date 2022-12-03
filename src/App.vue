607b19aa389328f6b5cb76e3a78e0f9f18d1f613
shoyaminakawa <s0620059@iwate-u.ac.jp>
2022-12-03 23:27:56 +0900

    書き換えた内容を一旦保存

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Home />
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/result">Result</router-link>
+  </div>
 </template>
 
+<script>
+import Home from "@/components/Home.vue"
+
+export default {
+  components: {
+    Home,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header-box">ページのヘッダーです</div>
+</template>
+
+<style scoped>
+.header-box {
+  /*背景*/
+  background-color: #dedede;
+  /*テキスト*/
+  text-align: center;
+}
+</style>
